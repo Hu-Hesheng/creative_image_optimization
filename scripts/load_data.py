5c15c7132fd7cad71b7433c79db512aa26784984
natty <natnaelmasresha@gmail.com>
2022-11-02 05:01:15 +0000

    complete data loader

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -7,4 +7,18 @@ import boto3
 class Loader():
     def __init__():
         pass
-    def load_csv(bucket:str,)
+    def load_csv(bucket_path:str,file_path:str,save=False):
+        with so.open(bucket_path, 'rb') as file_data:
+            with zipfile.ZipFile(file_data) as z:
+                with z.open(file_path) as zip_file_data:
+                    csv_data = zip_file_data.read().decode("utf-8")
+        
+        csvStringIO = StringIO(csv_data)
+        df = pd.read_csv(csvStringIO, sep=",", header=None)
+        df.columns = df.iloc[0,:]
+        df.drop(index=[0],inplace=True)
+
+        if save:
+            df.to_csv("../data/extracted.csv",index=False)
+
+        return df
