a27af3f11359c9ae6bff095f5d1882ca68823386
natty <natnaelmasresha@gmail.com>
2022-11-02 04:56:48 +0000

    add data loader class

---
+++ scripts/load_data.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import zipfile
+from io import StringIO
+import smart_open as so
+import boto3
+
+class Loader():
+    def __init__():
+        pass
+    def load_csv(bucket:str,)
