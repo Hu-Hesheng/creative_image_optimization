318bf1b6b7aab66d7ac7ab5cd26f309a584a102c
Amanuel3065 <amanuelzewdu21@gmail.com>
2022-11-07 09:55:21 +0300

    add dashboard

---
+++ app.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import streamlit as st
+
+sys.path.insert(0, './dashboard')
+
+from multiapp import MultiApp
+from applications import model, features_viz
+
+st.set_page_config(page_title="Feature Visualization", layout="wide")
+
+app = MultiApp()
+
+st.sidebar.markdown("""
+# features
+""")
+
+# Add all your application here
+app.add_app("features_viz", features_viz.app)
+""" app.add_app("experience_analytics", experience_analytics.app)
+app.add_app("satisfaction_analysis", satisfaction_analysis.app) """
+app.add_app("Model", model.app) 
+
+# The main app
+app.run()
