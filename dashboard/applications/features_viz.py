9a6ea0889ae890d6d99f9168773f80eaeb251ae5
Amanuel3065 <amanuelzewdu21@gmail.com>
2022-11-07 10:35:43 +0300

    modify dashboard

--- dashboard/applications/features_viz.py
+++ dashboard/applications/features_viz.py
@@ -13,12 +13,12 @@ def app():
 
     st.header("Object populations")
     st.subheader("most objects in an asset")
-    st.bar_chart(df_cleaned['all_objects_count'])
-
-    st.header("Object populations")
-    st.subheader("most objects in an asset")
     st.line_chart(df_cleaned['all_objects_count'])
 
+
+    st.header("Creatives with the widest CTA buttons")
+    st.subheader("Wide CTAs")
+    st.map(df_cleaned['cta_width'])
     
 
     st.header("Feature Importance")
